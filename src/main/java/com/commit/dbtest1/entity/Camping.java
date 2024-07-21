61249e6953f7b30873a50f843a843403fb1921fe
PCH <paekrng7@naver.com>
2024-07-21 22:06:18 +0900

    chore: Entity 수정

--- src/main/java/com/commit/dbtest1/entity/Camping.java
+++ src/main/java/com/commit/dbtest1/entity/Camping.java
@@ -1,25 +1,196 @@
 package com.commit.dbtest1.entity;
 
 import lombok.Getter;
-import jakarta.persistence.*;
+import lombok.Setter;
+import software.amazon.awssdk.enhanced.dynamodb.mapper.annotations.DynamoDbBean;
+import software.amazon.awssdk.enhanced.dynamodb.mapper.annotations.DynamoDbPartitionKey;
+import software.amazon.awssdk.enhanced.dynamodb.mapper.annotations.DynamoDbAttribute;
+import java.time.LocalDateTime;
 
-@Entity
+@DynamoDbBean
 @Getter
+@Setter
 public class Camping {
-    @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private Long campId;  // 캠핑장 ID
-    private String factNm;  // 시설 이름
-    private String lineIntro;  // 한 줄 소개
-    private String intro;  // 소개
-    private String doNm;  // 도 이름
-    private String sigunguNm;  // 시군구 이름
-    private String zipcode;  // 우편번호
-    private String addr1;  // 주소1
-    private String addr2;  // 주소2
-    private double mapX;  // 지도 X 좌표
-    private double mapY;  // 지도 Y 좌표
-    private String tel;  // 전화번호
-    private String homepage;  // 홈페이지
-    private String manageNmpr;  // 관리 인원
+
+    private Long campId;
+    private String campName;
+    private String lineIntro;
+    private String intro;
+    private String doName;
+    private String sigunguName;
+    private String postCode;
+    private String featureSummary;
+    private String induty;
+    private String addr;
+    private String addrDetails;
+    private Double mapX;
+    private Double mapY;
+    private String tel;
+    private String homepage;
+    private Integer staffCnt;
+    private LocalDateTime createdDate;
+    private LocalDateTime modifiedDate;
+
+    @DynamoDbPartitionKey
+    @DynamoDbAttribute("camp_id")
+    public Long getCampId() {
+        return campId;
+    }
+
+    public void setCampId(Long campId) {
+        this.campId = campId;
+    }
+
+    @DynamoDbAttribute("camp_name")
+    public String getCampName() {
+        return campName;
+    }
+
+    public void setCampName(String campName) {
+        this.campName = campName;
+    }
+
+    @DynamoDbAttribute("line_intro")
+    public String getLineIntro() {
+        return lineIntro;
+    }
+
+    public void setLineIntro(String lineIntro) {
+        this.lineIntro = lineIntro;
+    }
+
+    @DynamoDbAttribute("intro")
+    public String getIntro() {
+        return intro;
+    }
+
+    public void setIntro(String intro) {
+        this.intro = intro;
+    }
+
+    @DynamoDbAttribute("do_name")
+    public String getDoName() {
+        return doName;
+    }
+
+    public void setDoName(String doName) {
+        this.doName = doName;
+    }
+
+    @DynamoDbAttribute("sigungu_name")
+    public String getSigunguName() {
+        return sigunguName;
+    }
+
+    public void setSigunguName(String sigunguName) {
+        this.sigunguName = sigunguName;
+    }
+
+    @DynamoDbAttribute("post_code")
+    public String getPostCode() {
+        return postCode;
+    }
+
+    public void setPostCode(String postCode) {
+        this.postCode = postCode;
+    }
+
+    @DynamoDbAttribute("feature_summary")
+    public String getFeatureSummary() {
+        return featureSummary;
+    }
+
+    public void setFeatureSummary(String featureSummary) {
+        this.featureSummary = featureSummary;
+    }
+
+    @DynamoDbAttribute("induty")
+    public String getInduty() {
+        return induty;
+    }
+
+    public void setInduty(String induty) {
+        this.induty = induty;
+    }
+
+    @DynamoDbAttribute("addr")
+    public String getAddr() {
+        return addr;
+    }
+
+    public void setAddr(String addr) {
+        this.addr = addr;
+    }
+
+    @DynamoDbAttribute("addr_details")
+    public String getAddrDetails() {
+        return addrDetails;
+    }
+
+    public void setAddrDetails(String addrDetails) {
+        this.addrDetails = addrDetails;
+    }
+
+    @DynamoDbAttribute("mapX")
+    public Double getMapX() {
+        return mapX;
+    }
+
+    public void setMapX(Double mapX) {
+        this.mapX = mapX;
+    }
+
+    @DynamoDbAttribute("mapY")
+    public Double getMapY() {
+        return mapY;
+    }
+
+    public void setMapY(Double mapY) {
+        this.mapY = mapY;
+    }
+
+    @DynamoDbAttribute("tel")
+    public String getTel() {
+        return tel;
+    }
+
+    public void setTel(String tel) {
+        this.tel = tel;
+    }
+
+    @DynamoDbAttribute("homepage")
+    public String getHomepage() {
+        return homepage;
+    }
+
+    public void setHomepage(String homepage) {
+        this.homepage = homepage;
+    }
+
+    @DynamoDbAttribute("staff_cnt")
+    public Integer getStaffCnt() {
+        return staffCnt;
+    }
+
+    public void setStaffCnt(Integer staffCnt) {
+        this.staffCnt = staffCnt;
+    }
+
+    @DynamoDbAttribute("created_date")
+    public LocalDateTime getCreatedDate() {
+        return createdDate;
+    }
+
+    public void setCreatedDate(LocalDateTime createdDate) {
+        this.createdDate = createdDate;
+    }
+
+    @DynamoDbAttribute("modified_date")
+    public LocalDateTime getModifiedDate() {
+        return modifiedDate;
+    }
+
+    public void setModifiedDate(LocalDateTime modifiedDate) {
+        this.modifiedDate = modifiedDate;
+    }
 }
--- src/main/java/com/commit/dbtest1/repository/CampingRepository.java
+++ src/main/java/com/commit/dbtest1/repository/CampingRepository.java
@@ -2,8 +2,8 @@ package com.commit.dbtest1.repository;
 
 import com.commit.dbtest1.entity.Camping;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface CampingRepository extends JpaRepository<Camping, Long> {
-    
 }
-
